d6115bf1544450c6565dc78ae63c98e0f809dd00
artur-hurynovich <artur.hurynovich@gmail.com>
2019-01-17 14:30:53 +0300

    added sign up and sign in forms

--- src/main/java/by/hurynovich/mus_overview/vaadin/form/impl/SignInForm.java
+++ src/main/java/by/hurynovich/mus_overview/vaadin/form/impl/SignInForm.java
@@ -1,68 +1,76 @@
 package by.hurynovich.mus_overview.vaadin.form.impl;
 
 import by.hurynovich.mus_overview.dto.impl.UserDTO;
-import by.hurynovich.mus_overview.service.impl.UserService;
+import by.hurynovich.mus_overview.service.IUserDTOService;
 import by.hurynovich.mus_overview.vaadin.form.AbstractDTOForm;
-import com.vaadin.data.Binder;
 import com.vaadin.data.ValidationResult;
 import com.vaadin.data.validator.EmailValidator;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+import javax.annotation.PostConstruct;
 import java.util.stream.Collectors;
 
 public class SignInForm extends AbstractDTOForm<UserDTO> {
+
+    @Autowired
+    @Qualifier("userService")
+    private IUserDTOService userService;
+
+    private UserDTO userDTO;
 
     private TextField emailField;
 
     private PasswordField passwordField;
 
-    private Binder<UserDTO> binder;
-
     private Button signInButton;
 
-    private UserService userService;
-
-    public SignInForm() {
-        /*this.userService = userService;
-        emailField = new TextField("E-mail:");
-        passwordField = new PasswordField("Password:");
-        binder = getBinder();
-        binder.readBean(userDTO);
-        addComponents(emailField, passwordField, getSignInButton(userDTO));*/
+    @PostConstruct
+    public void init() {
+        userDTO = new UserDTO();
+        setupBinder();
+        setupParentLayout();
+        setContent(getParentLayout());
     }
 
-    public Binder<UserDTO> getBinder() {
-        if (binder == null) {
-            binder = new Binder<>(UserDTO.class);
-            binder.forField(emailField).withValidator(new EmailValidator("Please enter a valid e-mail!")).
-                    bind(UserDTO::getEmail, UserDTO::setEmail);
-            binder.forField(passwordField).withValidator(password -> password != null && password.length() > 7,
-                    "Please enter a password with minimal length of 8 characters!").
-                    bind(UserDTO::getPassword, UserDTO::setPassword);
+    private void setupParentLayout() {
+        getParentLayout().addComponents(getEmailField(), getPasswordField(), getSignInButton());
+    }
+
+    private TextField getEmailField() {
+        if (emailField == null) {
+            emailField = new TextField("E-mail:");
         }
-        return binder;
+        return emailField;
     }
 
-    private Button getSignInButton(final UserDTO userDTO) {
+    private PasswordField getPasswordField() {
+        if (passwordField == null) {
+            passwordField = new PasswordField("Password:");
+        }
+        return passwordField;
+    }
+
+    private Button getSignInButton() {
         if (signInButton == null) {
             signInButton = new Button("Sign In");
             signInButton.addClickListener(clickEvent -> {
-                if (binder.writeBeanIfValid(userDTO)) {
+                if (getBinder().writeBeanIfValid(userDTO)) {
                     final UserDTO signedInUser = userService.findByEmailAndPassword(userDTO);
                     if (signedInUser == null) {
-                        Notification.show("Warning!\n" + "Incorrect E-mail or/and Password!",
+                        Notification.show("Warning!\nIncorrect E-mail or/and Password!",
                                 Notification.Type.WARNING_MESSAGE);
                     } else {
                         Notification.show("User signed in!",
                                 Notification.Type.HUMANIZED_MESSAGE);
                     }
                 } else {
-                    final String validationError = binder.validate().getValidationErrors().stream().
-                            map(ValidationResult::getErrorMessage).collect(Collectors.joining("; "));
+                    final String validationError = getBinder().validate().getValidationErrors().stream().
+                            map(ValidationResult::getErrorMessage).collect(Collectors.joining(";\n"));
                     Notification.show("Warning!\n" + validationError,
                             Notification.Type.WARNING_MESSAGE);
                 }
@@ -71,8 +79,17 @@ public class SignInForm extends AbstractDTOForm<UserDTO> {
         return signInButton;
     }
 
+    private void setupBinder() {
+        getBinder().readBean(userDTO);
+        getBinder().forField(getEmailField()).
+                withValidator(new EmailValidator("Please, enter a valid e-mail!")).
+                bind(UserDTO::getEmail, UserDTO::setEmail);
+        getBinder().forField(getPasswordField()).withValidator(password -> password != null && password.length() > 7,
+                "Please, enter a password with minimal length of 8 characters!").
+                bind(UserDTO::getPassword, UserDTO::setPassword);
+    }
+
     @Override
     public void setupForm(final UserDTO userDTO, final Runnable onSave, final Runnable inDiscard) {
-
     }
 }
--- src/main/java/by/hurynovich/mus_overview/vaadin/form/impl/SignUpForm.java
+++ src/main/java/by/hurynovich/mus_overview/vaadin/form/impl/SignUpForm.java
@@ -1,22 +1,29 @@
 package by.hurynovich.mus_overview.vaadin.form.impl;
 
 import by.hurynovich.mus_overview.dto.impl.UserDTO;
-import by.hurynovich.mus_overview.service.impl.UserService;
+import by.hurynovich.mus_overview.service.IUserDTOService;
 import by.hurynovich.mus_overview.vaadin.custom_field.UserRoleField;
 import by.hurynovich.mus_overview.vaadin.form.AbstractDTOForm;
-import com.vaadin.data.Binder;
 import com.vaadin.data.ValidationResult;
 import com.vaadin.data.validator.EmailValidator;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+import javax.annotation.PostConstruct;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class SignUpForm extends AbstractDTOForm<UserDTO> {
+
+    @Autowired
+    @Qualifier("userService")
+    private IUserDTOService userService;
+
+    private UserDTO userDTO;
 
     private TextField nameField;
 
@@ -26,58 +33,68 @@ public class SignUpForm extends AbstractDTOForm<UserDTO> {
 
     private UserRoleField userRoleField;
 
-    private Binder<UserDTO> binder;
-
     private Button signUpButton;
 
-    private UserService userService;
-
-    public SignUpForm() {
-        /*this.userService = userService;
-        nameField = new TextField("Name:");
-        emailField = new TextField("E-mail:");
-        passwordField = new PasswordField("Password:");
-        userRoleField = new UserRoleField("User Role:");
-        binder = getBinder();
-        binder.readBean(userDTO);
-        addComponents(nameField, emailField, passwordField, userRoleField, getSignUpButton(userDTO));*/
+    @PostConstruct
+    public void init() {
+        userDTO = new UserDTO();
+        setupBinder();
+        setupParentLayout();
+        setContent(getParentLayout());
     }
 
-    public Binder<UserDTO> getBinder() {
-        if (binder == null) {
-            binder = new Binder<>(UserDTO.class);
-            binder.forField(nameField).withValidator(name -> name != null && name.length() > 4,
-                    "Please enter a name with minimal length of 5 characters!").
-                    bind(UserDTO::getName, UserDTO::setName);
-            binder.forField(emailField).withValidator(new EmailValidator("Please enter a valid e-mail!")).
-                    bind(UserDTO::getEmail, UserDTO::setEmail);
-            binder.forField(passwordField).withValidator(password -> password != null && password.length() > 7,
-                    "Please enter a password with minimal length of 8 characters!").
-                    bind(UserDTO::getPassword, UserDTO::setPassword);
-            binder.forField(userRoleField).withValidator(Objects::nonNull, "Please choose user role!").
-                    bind(UserDTO::getRole, UserDTO::setRole);
+    private void setupParentLayout() {
+        getParentLayout().addComponents(getNameField(), getEmailField(), getPasswordField(), getUserRoleField(),
+                getSignUpButton());
+    }
+
+    private TextField getNameField() {
+        if (nameField == null) {
+            nameField = new TextField("Name:");
         }
-        return binder;
+        return nameField;
     }
 
-    private Button getSignUpButton(final UserDTO userDTO) {
+    private TextField getEmailField() {
+        if (emailField == null) {
+            emailField = new TextField("E-mail:");
+        }
+        return emailField;
+    }
+
+    private PasswordField getPasswordField() {
+        if (passwordField == null) {
+            passwordField = new PasswordField("Password:");
+        }
+        return passwordField;
+    }
+
+    private UserRoleField getUserRoleField() {
+        if (userRoleField == null) {
+            userRoleField = new UserRoleField();
+            userRoleField.setCaption("Role:");
+        }
+        return userRoleField;
+    }
+
+    private Button getSignUpButton() {
         if (signUpButton == null) {
             signUpButton = new Button("Sign Up");
             signUpButton.addClickListener(clickEvent -> {
-                if (binder.writeBeanIfValid(userDTO)) {
+                if (getBinder().writeBeanIfValid(userDTO)) {
                     final String email = userDTO.getEmail();
                     if (userService.emailExists(email)) {
                         Notification.show("Warning!\n" + "User with E-mail \'" + userDTO.getEmail() +
-                                "\' already exists!",
+                                        "\' already exists!",
                                 Notification.Type.WARNING_MESSAGE);
                     } else {
                         userService.save(userDTO);
-                        Notification.show("User \'" + userDTO.getName() + "\' signed up!",
+                        Notification.show("User signed up!",
                                 Notification.Type.HUMANIZED_MESSAGE);
                     }
                 } else {
-                    final String validationError = binder.validate().getValidationErrors().stream().
-                            map(ValidationResult::getErrorMessage).collect(Collectors.joining("; "));
+                    final String validationError = getBinder().validate().getValidationErrors().stream().
+                            map(ValidationResult::getErrorMessage).collect(Collectors.joining(";\n"));
                     Notification.show("Warning!\n" + validationError,
                             Notification.Type.WARNING_MESSAGE);
                 }
@@ -86,8 +103,23 @@ public class SignUpForm extends AbstractDTOForm<UserDTO> {
         return signUpButton;
     }
 
+    private void setupBinder() {
+        getBinder().readBean(new UserDTO());
+        getBinder().forField(getNameField()).withValidator(name -> name != null && name.length() > 4,
+                "Please, enter a name with minimal length of 5 characters!").
+                bind(UserDTO::getName, UserDTO::setName);
+        getBinder().forField(getEmailField()).
+                withValidator(new EmailValidator("Please, enter a valid e-mail!")).
+                bind(UserDTO::getEmail, UserDTO::setEmail);
+        getBinder().forField(getPasswordField()).withValidator(password -> password != null && password.length() > 7,
+                "Please, enter a password with minimal length of 8 characters!").
+                bind(UserDTO::getPassword, UserDTO::setPassword);
+        getBinder().forField(getUserRoleField()).
+                withValidator(Objects::nonNull, "Please, select user role!").
+                bind(UserDTO::getRole, UserDTO::setRole);
+    }
+
     @Override
     public void setupForm(final UserDTO userDTO, final Runnable onSave, final Runnable inDiscard) {
-
     }
 }
